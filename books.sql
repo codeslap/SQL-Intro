da311f257608cea44284d24e0fe6f461de17ecf5
codeslap <56369460+codeslap@users.noreply.github.com>
2022-07-22 14:47:33 -0700

    Create books.sql

---
+++ books.sql
@@ -0,0 +1,11 @@
+/* This Project is a list of my favorite books with the author and a rating out of 5.
+CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, author TEXT, rating REAL);
+
+INSERT INTO books VALUES (1,"The BFG", "Roald Dahl", 4.1);
+INSERT INTO books VALUES (2,"Outliers", "Malcolm Gladwell", 4.2);
+INSERT INTO books VALUES (3, "Harry Potter Series", "J.K. Rowling",4.6);
+
+INSERT INTO books (name, author, rating) VALUES ("Leven Thumps and the Gateway to Foo", "Obert Sky", 4.2);
+
+INSERT INTO books (name, author, rating) VALUES ("Human Geography: People, Place, and Culture", "Erin Fouberg, Alexander Murphy", 3.9);
+SELECT * FROM books;
