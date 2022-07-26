60aba3dc2c2f8434bcb515e490e3f9c42efa0b79
codeslap <56369460+codeslap@users.noreply.github.com>
2022-07-26 12:56:49 -0700

    Update books.sql

--- books.sql
+++ books.sql
@@ -7,5 +7,10 @@ INSERT INTO books VALUES (3, "Harry Potter Series", "J.K. Rowling",4.6);
 INSERT INTO books (name, author, rating) VALUES ("Leven Thumps and the Gateway to Foo", "Obert Sky", 4.2);
 
 INSERT INTO books (name, author, rating) VALUES ("Human Geography: People, Place, and Culture", "Erin Fouberg, Alexander Murphy", 3.9);
-SELECT * FROM books;
 
+SELECT * FROM books; /* Lists all books, author, and rating in table
+
+/* This is a list of my favorite books, with the author, and rating. 
+
+SELECT * FROM books WHERE rating > 4.1; /* You can also use this to list books above a certain rating
+such as greater than 4.1 in line 15.
